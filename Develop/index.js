445b1a5c70e5d168387e3fb760cc7ad32beae3e9
Tommy McG <thomasjmcgovern98@gmail.com>
2022-04-19 18:57:37 -0400

    very basic layout

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,68 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project title?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Give your project a description:'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Please provide installation instructions:'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please provide some usage information:'
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Please provide contribution guidelines'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Please provide test instructions'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What license do you have for this application?',
+        choice: ['Drivers', 'Boating', 'Home improvement']
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, JSON.stringify(data), () => {
+
+    })
+    console.log(data);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then((answers) => {
+        writeToFile('NewReadMe.txt', answers);
+    })
+    .catch((error) => {
+        if(error.isTtyError) {
+            console.log("Prompt can't be rendered in current environment");
+        } else {
+            console.log("Something else went wrong.");
+        }
+    })
+}
 
 // Function call to initialize app
 init();
---
+++ Develop/src/readme-template.js
@@ -0,0 +1,7 @@
+const generateReadMe = (name) => {
+    return `
+    
+    `;
+}
+
+module.exports = generateReadMe;
